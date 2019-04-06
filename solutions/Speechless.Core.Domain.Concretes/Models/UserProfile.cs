4e25b3728c2a269ff7e74ed70a606dc353c706a6
Emmanuel Ngwane <ngwanem@hotmail.com>
2019-04-06 23:33:59 +0200

    Changed setup on mobile applications.

---
+++ solutions/Speechless.Core.Domain.Concretes/Models/UserProfile.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reflektiv.Speechless.Core.Domain.Concretes.Models
+{
+    class UserProfile
+    {
+    }
+}
---
+++ solutions/Speechless.Mobile.SpeechlessApp/Speechless.Mobile.SpeechlessApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Speechless.Mobile.SpeechlessApp.Models;
+
+namespace Speechless.Mobile.SpeechlessApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
