1ad5b7938c21c0975112e348f9f680f849418f75
Emmanuel Ngwane <ngwanem@hotmail.com>
2022-08-24 23:03:15 +0200

    Added presentation layer and MAUI support

---
+++ solutions/SpeechlessApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SpeechlessApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ solutions/SpeechlessApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SpeechlessApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
