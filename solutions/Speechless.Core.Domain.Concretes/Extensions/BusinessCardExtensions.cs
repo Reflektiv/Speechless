5f2802f455eae21dd4de9d383983628d688266cb
Emmanuel Ngwane <ngwanem@hotmail.com>
2022-08-26 09:28:22 +0200

    Added XML support

--- solutions/Speechless.Core.Domain.Concretes/Extensions/BusinessCardExtensions.cs
+++ solutions/Speechless.Core.Domain.Concretes/Extensions/BusinessCardExtensions.cs
@@ -1,61 +1,104 @@
 ﻿using MixERP.Net.VCards;
-using MixERP.Net.VCards.Models;
 using Reflektiv.Speechless.Core.Domain.Concretes.Models;
-using ServiceStack;
 using System;
 using System.Collections.Generic;
-using Language = MixERP.Net.VCards.Models.Language;
 
 namespace Reflektiv.Speechless.Core.Domain.Concretes.Extensions
 {
     public static class BusinessCardExtensions
     {
-        public static BusinessCard ToBusinessCard(this VCard card)
+        public static void PopulateFrom(this BusinessCard card, VCard vcard)
+        {
+            card.FirstName = vcard.FirstName;
+            card.MiddleName = vcard.MiddleName;
+            card.Prefix = vcard.Prefix;
+            card.Suffix = vcard.Suffix;
+            card.NickName = vcard.NickName;
+            card.SortString = vcard.SortString;
+            card.Sound = vcard.Sound;
+            card.LastName = vcard.LastName;
+            card.Key = vcard.Key;
+            card.Source = vcard.Source;
+            card.Kind = vcard.Kind;
+            card.Anniversary = vcard.Anniversary;
+            card.Gender = vcard.Gender;
+            card.FormattedName = vcard.FormattedName;
+            card.Longitude = vcard.Longitude;
+            card.BirthDay = vcard.BirthDay;
+            card.Mailer = vcard.Mailer;
+            card.Title = vcard.Title;
+            card.Role = vcard.Role;
+            card.Latitude = vcard.Latitude;
+            card.TimeZone = vcard.TimeZone;
+            card.Photo = vcard.Photo;
+            card.Note = vcard.Note;
+            card.LastRevision = vcard.LastRevision;
+            card.Url = vcard.Url;
+            card.UniqueIdentifier = vcard.UniqueIdentifier;
+            card.Version = vcard.Version;
+            card.Organization = vcard.Organization;
+            card.OrganizationalUnit = vcard.OrganizationalUnit;
+            card.Classification = vcard.Classification;
+            card.DeliveryAddress = vcard.DeliveryAddress;
+            card.Logo = vcard.Logo;
+            card.Categories = vcard.Categories;
+            card.Impps = vcard.Impps;
+            card.Languages = vcard.Languages;
+            card.Relations = vcard.Relations;
+            card.CalendarUserAddresses = vcard.CalendarUserAddresses;
+            card.CalendarAddresses = vcard.CalendarAddresses;
+            card.Addresses = vcard.Addresses;
+            card.Telephones = vcard.Telephones;
+            card.Emails = vcard.Emails;
+            card.CustomExtensions = vcard.CustomExtensions;
+        }
+
+        public static BusinessCard ToBusinessCard(this VCard vcard)
         {
             return new BusinessCard
             {
-                FirstName = card.FirstName,
-                MiddleName = card.MiddleName,
-                Prefix = card.Prefix,
-                Suffix = card.Suffix,
-                NickName = card.NickName,
-                SortString = card.SortString,
-                Sound = card.Sound,
-                LastName = card.LastName,
-                Key = card.Key,
-                Source = card.Source,
-                Kind = card.Kind,
-                Anniversary = card.Anniversary,
-                Gender = card.Gender,
-                FormattedName = card.FormattedName,
-                Longitude = card.Longitude,
-                BirthDay = card.BirthDay,
-                Mailer = card.Mailer,
-                Title = card.Title,
-                Role = card.Role,
-                Latitude = card.Latitude,
-                TimeZone = card.TimeZone,
-                Photo = card.Photo,
-                Note = card.Note,
-                LastRevision = card.LastRevision,
-                Url = card.Url,
-                UniqueIdentifier = card.UniqueIdentifier,
-                Version = card.Version,
-                Organization = card.Organization,
-                OrganizationalUnit = card.OrganizationalUnit,
-                Classification = card.Classification,
-                DeliveryAddress = card.DeliveryAddress,
-                Logo = card.Logo,
-                Categories = card.Categories,
-                Impps = card.Impps,
-                Languages = card.Languages,
-                Relations = card.Relations,
-                CalendarUserAddresses = card.CalendarUserAddresses,
-                CalendarAddresses = card.CalendarAddresses,
-                Addresses = card.Addresses,
-                Telephones = card.Telephones,
-                Emails = card.Emails,
-                CustomExtensions = card.CustomExtensions,
+                FirstName = vcard.FirstName,
+                MiddleName = vcard.MiddleName,
+                Prefix = vcard.Prefix,
+                Suffix = vcard.Suffix,
+                NickName = vcard.NickName,
+                SortString = vcard.SortString,
+                Sound = vcard.Sound,
+                LastName = vcard.LastName,
+                Key = vcard.Key,
+                Source = vcard.Source,
+                Kind = vcard.Kind,
+                Anniversary = vcard.Anniversary,
+                Gender = vcard.Gender,
+                FormattedName = vcard.FormattedName,
+                Longitude = vcard.Longitude,
+                BirthDay = vcard.BirthDay,
+                Mailer = vcard.Mailer,
+                Title = vcard.Title,
+                Role = vcard.Role,
+                Latitude = vcard.Latitude,
+                TimeZone = vcard.TimeZone,
+                Photo = vcard.Photo,
+                Note = vcard.Note,
+                LastRevision = vcard.LastRevision,
+                Url = vcard.Url,
+                UniqueIdentifier = vcard.UniqueIdentifier,
+                Version = vcard.Version,
+                Organization = vcard.Organization,
+                OrganizationalUnit = vcard.OrganizationalUnit,
+                Classification = vcard.Classification,
+                DeliveryAddress = vcard.DeliveryAddress,
+                Logo = vcard.Logo,
+                Categories = vcard.Categories,
+                Impps = vcard.Impps,
+                Languages = vcard.Languages,
+                Relations = vcard.Relations,
+                CalendarUserAddresses = vcard.CalendarUserAddresses,
+                CalendarAddresses = vcard.CalendarAddresses,
+                Addresses = vcard.Addresses,
+                Telephones = vcard.Telephones,
+                Emails = vcard.Emails,
+                CustomExtensions = vcard.CustomExtensions,
             };
         }
 
--- solutions/Speechless.Core.Domain.Concretes/Models/BusinessCard.cs
+++ solutions/Speechless.Core.Domain.Concretes/Models/BusinessCard.cs
@@ -1,16 +1,26 @@
 ﻿using MixERP.Net.VCards;
+using MixERP.Net.VCards.Serializer;
+using reexmonkey.xmisc.core.system.xmltools.extensions;
+using Reflektiv.Speechless.Core.Domain.Concretes.Extensions;
 using Reflektiv.Speechless.Core.Domain.Concretes.Interfaces;
+using ServiceStack;
 using ServiceStack.Model;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
 
 namespace Reflektiv.Speechless.Core.Domain.Concretes.Models
 {
     /// <summary>
     /// Represents a business contact card.
     /// </summary>
-    public class BusinessCard : VCard, IHasId<Guid>, ITrashable, IEquatable<BusinessCard>
+    public class BusinessCard : VCard, IHasId<Guid>, ITrashable, IEquatable<BusinessCard>, IXmlSerializable
     {
+        public const string Namespace = "https://github.com/reflektiv/speechless";
+
         /// <summary>
         /// The unique identifier of the business card.
         /// </summary>
@@ -78,6 +88,83 @@ namespace Reflektiv.Speechless.Core.Domain.Concretes.Models
         }
 
         public override int GetHashCode() => HashCode.Combine(Title, LastName, FirstName, MiddleName, Prefix, Suffix, Id);
+
+        public XmlSchema? GetSchema() => null;
+
+        public void ReadXml(XmlReader reader)
+        {
+            var subreader = reader.ReadSubtree();
+
+            if (subreader.LocalName.EqualsIgnoreCase(nameof(BusinessCard)))
+            {
+                for (int i = 0; i < subreader.AttributeCount; i++)
+                {
+                    reader.MoveToAttribute(i);
+                    if (subreader.LocalName.EqualsIgnoreCase("id"))
+                    {
+                        Id = new Guid(subreader.Value);
+                        break;
+                    }
+                }
+            }
+
+            while (subreader.Read())
+            {
+                if (subreader.NodeType != XmlNodeType.Element) continue;
+
+                if (reader.LocalName.EqualsIgnoreCase(nameof(DisplayName)))
+                    DisplayName = reader.SafeReadElementContentAsString();
+
+                if (reader.LocalName.EqualsIgnoreCase(nameof(CreatedDate)))
+                    CreatedDate = reader.SafeReadElementContentAsNullableDateTime(XmlDateTimeSerializationMode.Utc);
+
+                if (reader.LocalName.EqualsIgnoreCase(nameof(LastModifiedDate)))
+                    LastModifiedDate = reader.SafeReadElementContentAsNullableDateTime(XmlDateTimeSerializationMode.Utc);
+
+                if (reader.LocalName.EqualsIgnoreCase(nameof(ExpiryDate)))
+                    ExpiryDate = reader.SafeReadElementContentAsNullableDateTime(XmlDateTimeSerializationMode.Utc);
+
+                if (reader.LocalName.EqualsIgnoreCase(nameof(Tags)))
+                {
+                    var value = reader.SafeReadElementContentAsString();
+                    var tags = value.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                    foreach (var tag in tags) Tags.Add(tag);
+                }
+
+                if (reader.LocalName.EqualsIgnoreCase(nameof(IsTrashed)))
+                    IsTrashed = reader.SafeReadElementContentAsBoolean();
+
+                if (reader.LocalName.EqualsIgnoreCase(nameof(VCard)))
+                {
+                    var content = reader.SafeReadElementContentAsString();
+                    var vcard = Deserializer.GetVCard(content);
+                    if (vcard != null) this.PopulateFrom(vcard);
+                }
+            }
+        }
+
+        public void WriteXml(XmlWriter writer)
+        {
+            const string prefix = "bc";
+
+            writer.SafeWriteAttributeString(prefix, nameof(BusinessCard), Namespace, Id.ToString());
+            if (!string.IsNullOrEmpty(DisplayName))
+                writer.SafeWriteElementString(prefix, nameof(DisplayName), Namespace, DisplayName);
+            if (CreatedDate.HasValue)
+                writer.SafeWriteElementString(prefix, nameof(CreatedDate), Namespace, CreatedDate.Value, XmlDateTimeSerializationMode.Utc);
+            if (LastModifiedDate.HasValue)
+                writer.SafeWriteElementString(prefix, nameof(LastModifiedDate), Namespace, LastModifiedDate.Value, XmlDateTimeSerializationMode.Utc);
+            if (ExpiryDate.HasValue)
+                writer.SafeWriteElementString(prefix, nameof(ExpiryDate), Namespace, ExpiryDate.Value, XmlDateTimeSerializationMode.Utc);
+            if (Tags.Any())
+                writer.SafeWriteElementString(prefix, nameof(Tags), Namespace, string.Join(';', Tags));
+            if (IsTrashed)
+                writer.SafeWriteElementString(prefix, nameof(IsTrashed), Namespace, IsTrashed);
+
+            var vcard = this.Serialize();
+            if (!string.IsNullOrEmpty(vcard))
+                writer.SafeWriteElementString(prefix, nameof(VCard), Namespace, vcard);
+        }
 
         public static bool operator ==(BusinessCard? left, BusinessCard? right) => EqualityComparer<BusinessCard>.Default.Equals(left, right);
 
